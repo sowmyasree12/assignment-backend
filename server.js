ff112048d6c9a08a18fc6f49bfe1f6f40a2d242d
Sowmya Sree <sowmyasree12@gmail.com>
2026-02-18 18:46:44 +0530

    Fix Render port issue

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -53,5 +54,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
