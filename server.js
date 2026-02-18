870a2fec34fe4abcfc092e0588188f74990ee241
Sowmya Sree <sowmyasree12@gmail.com>
2026-02-18 19:45:46 +0530

    Fix Render port binding

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
 console.log("Starting backend...");
 
 let submissions = [];
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
 
 app.post("/submit", (req, res) => {
   const { assignmentId, studentId, content } = req.body;
@@ -49,10 +52,7 @@ app.post("/submit", (req, res) => {
 app.get("/submissions", (req, res) => {
   res.json(submissions);
 });
-app.get("/", (req, res) => {
-  res.send("Backend is running successfully 🚀");
-});
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on port ${PORT}`);
 });
